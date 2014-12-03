a301c2266a36fd7a39c3fd164db89325f10244f2
LogoiLab <cuddlycactusmail@gmail.com>
2014-12-02 19:06:32 -0500

    fixes

--- Install/Downer.ps1
+++ Install/Downer.ps1
@@ -54,6 +54,10 @@ Download-Item -url $dlurl -targetfile $dlfile
 Decompress-Zips -zippath $downloadir
 #Remove the downloaded zip file (copy protection and disk space)
 Remove-Item "$downloadir\$zipname" -Force
+Move-Item -Path "$downloadir\PowerCraft-Server-Manager-master\*" -Destination $downloadir -Force
+Remove-Item "$downloadir\PowerCraft-Server-Manager-master" -Force -Recurse
+Remove-Item "$downloadir\.gitignore" -Force
+Remove-Item "$downloadir\.gitattributes" -Force
 # Create a PowerCraft Shortcut with Windows PowerShell
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\Powercraft.lnk")
--- Install/PowerCraft-Server-Manager-master/PowerCraft/Powercraft.ps1
+++
@@ -1,3 +0,0 @@
-﻿if($ver = "1.7.10"){
-$dlurl = "https://s3.amazonaws.com/Minecraft.Download/versions/$ver/minecraft_server.$ver.jar"
-}
